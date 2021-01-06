594feafefea1c1943412c7a3582be9c64e976c49
zxc91911003 <zxc91911003@gmail.com>
2021-01-06 10:53:44 +0000

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from indeed import extract_indeed_pages, extract_indeed_jobs
+from save import save_to_file
 
 last_indeed_page = extract_indeed_pages()
 indeed_jobs = extract_indeed_jobs(last_indeed_page)
-print(indeed_jobs)
+save_to_file(indeed_jobs)
